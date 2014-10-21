26b827c35e58516d72f7ee729a5600c74e8e7e2d
Andrew Linville <andlinville@gmail.com>
2014-10-20 22:26:03 -0500

    license and typo

--- js/bdOutput.js
+++ js/bdOutput.js
@@ -1,6 +1,6 @@
 // The MIT License (MIT)
 
-// thinker.js / Copyright (c) 2014 Andrew Linville
+// brain-dump / Copyright (c) 2014 Andrew Linville
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
